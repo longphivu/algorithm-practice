accb22c8222182a86f236f722413813712ca0d0f
vulong <vulong@>
2016-07-11 10:05:05 -0400

    leetcode - Q21 Merge 2 sorted list

---
+++ Algorithm/src/leetcode/Q21_MergeTwoSortList.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import universal.ListNode;
+
+public class Q21_MergeTwoSortList {
+	/**
+	 * Definition for singly-linked list. public class ListNode { int val;
+	 * ListNode next; ListNode(int x) { val = x; next = null; } }
+	 */
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		ListNode head = new ListNode(0);
+		ListNode headcopy = head;
+		while (l1 != null && l2 != null) {
+			if (l1.val < l2.val) {
+				headcopy.next = l1;
+				l1 = l1.next;
+			} else if (l2.val <= l1.val) {
+				headcopy.next = l2;
+				l2 = l2.next;
+			}
+			headcopy = headcopy.next;
+		}
+
+		headcopy.next = (l1 == null ? l2 : l1);
+		return head.next;
+	}
+}
