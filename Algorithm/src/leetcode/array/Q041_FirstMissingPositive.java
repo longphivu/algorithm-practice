a1a3d3c6eaf6853e5712ce91ab0cf4ced7ecc50c
longvu <longvu.cs@outlook.com>
2016-11-02 16:34:32 -0400

    leetcode Update Q41_MissingPositive: add constant space solution

--- Algorithm/src/leetcode/array/Q041_FirstMissingPositive.java
+++ Algorithm/src/leetcode/array/Q041_FirstMissingPositive.java
@@ -12,9 +12,9 @@ package leetcode.array;
 				and [3,4,-1,1] return 2.
 				Your algorithm should run in O(n) time and uses constant space. 
  *
- * Solution:	
- * Complexity:
- * Notes:
+ * Solution:	Use additional space to store the number appearance
+ * Complexity:	O(n)
+ * Notes:		Constant space solution: swap number to their respective position, if outside range leave it
  *				
  * Follow up:	
  */
@@ -35,4 +35,43 @@ public class Q041_FirstMissingPositive {
 		}
 		return n + 1;
 	}
+	
+	public static int firstMissingPositive2(int[] nums) {
+        if (nums == null || nums.length == 0) return 1;
+        
+        int last = nums.length-1;
+        for (int i = 0; i < nums.length && last >= 0; i++) {
+            int cur = nums[i];
+            //ignore these case:
+            //1. outside range
+            //2. already at correct position: cur == i+1
+            //3. another number already there: nums[cur-1] == cur ,eg. [1,1]
+            while (cur > 0 && cur <= nums.length && cur != i+1 && nums[cur-1] != cur) {
+                swap(nums, i, cur-1);
+                cur = nums[i];
+            }
+        }
+        
+        int result = 0;
+        
+        for(int i = 0; i < nums.length; i++) {
+            if (nums[i] != i+1) {
+                result = i+1;
+                break;
+            }
+        }
+        
+        return result == 0 ? nums.length+1 : result;
+    }
+    
+    private static void swap(int[] nums, int from, int to) {
+        int temp = nums[from];
+        nums[from] = nums[to];
+        nums[to] = temp;
+    }
+    
+    public static void main(String...stri) {
+    	int[] arr = {3,0,1,1};
+    	System.out.println(firstMissingPositive2(arr));
+    }
 }
