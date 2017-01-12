a1d251195aee469551860d3a12c583bc67a6bb78
longvu <longvu.cs@outlook.com>
2017-01-12 16:58:06 -0500

    hackerrank Add Utility class

--- Algorithm/src/hackerrank/ScannerUtility.java
+++ Algorithm/src/hackerrank/ScannerUtility.java
@@ -15,4 +15,17 @@ public class ScannerUtility {
         }
         return a;
 	}
+	
+	public static void swap(int[] arr, int i, int j) {
+		int t = arr[i];
+		arr[i] = arr[j];
+		arr[j] = t;
+	}
+	
+	public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
 }
